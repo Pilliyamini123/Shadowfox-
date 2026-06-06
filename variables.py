9f0c3e380e837982502c8f2c627015c4047428a8
Pilliyamini123 <pilliyamini650@gmail.com>
2026-06-06 18:38:54 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+#question 1
+#CREATE A VARIABLE pi and print its type
+pi=22/7
+print("value of pi:",pi)
+print("type of pi:",type(pi))
+#question 2
+#why cant we use "for" as variable?
+#for =4
+#reason:
+#"for" is a reserved keyword and cannot be used as variable name
+print("\nquestion2")
+print(" 'for' is a reserved keyword and cannot be used as variable name")
+#question3
+#simple interest
+#si=(p*t*r)/100
+p=10000
+r=2
+t=3
+si=(p*t*r)/100
+print("simple interest is",si)
